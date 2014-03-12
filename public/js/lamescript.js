3a7f811c6a16a70c21ffa09a2efc141638f829cf
jahsan87 <jahsan87@gmail.com>
2014-03-11 15:50:34 -1000

    added more lame validations/sanitizers

--- public/js/lamescript.js
+++ public/js/lamescript.js
@@ -1,11 +1,44 @@
 $(function(){
-  
+
+  $('textarea').keyup(function(e) {
+    if ($('textarea').val().split('').indexOf('>') !==-1 || $('textarea').val().split('').indexOf('<') !==-1) {
+      alert("You are not allowed to use the > < symbols");
+      $('#comment').val('');
+      e.preventDefault();
+    }
+  });
+
+  $('#name').keyup(function(e) {
+    if($('#name').val().split('').indexOf('>') !==-1 || $('#name').val().split('').indexOf('<') !==-1){
+      alert("You are not allowed to use the > < symbols.");
+      $('#name').val('');
+      e.preventDefault();
+    }
+  });
+
+    $('#email').keyup(function(e) {
+       if($('#email').val().split('').indexOf('>') !==-1 || $('#email').val().split('').indexOf('<') !==-1){
+         alert("You are not allowed to use the > < symbols.");
+         $('#email').val('');
+         e.preventDefault();
+    }
+  });
+
   // lame validation
   $('#submit').click(function(e){
+
     if($('#name').val().trim() == "" || $('#email').val().trim() == ""){
       alert("You must enter your name and email.");
       e.preventDefault();
     }
+
+    var fixComment = $('textarea').val();
+    fixComment = fixComment.replace('&','&amp').replace('"','&quot').replace("'",'&rsquo').replace("'",'&lsquo');
+
+    var fixName = $('#name').val();
+    fixName = fixName.replace('&','&amp').replace('"','&quot').replace("'",'&rsquo').replace("'",'&lsquo');
+    $('#name').val(fixName);
+
   });
 
 });
